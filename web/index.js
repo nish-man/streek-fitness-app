81b2f875613274fe2a1291523bd8ccef73cb7631
Nishant Mantripragada <nishantmantripragada@Nishants-MacBook-Air.local>
2025-03-25 01:12:00 +0530

    Update dependencies and web configuration for proper deployment

--- web/index.js
+++ web/index.js
@@ -1,12 +1,4 @@
-import { createRoot } from 'react-dom/client';
+import { registerRootComponent } from 'expo/build/launch/registerRootComponent';
 import App from '../App';
-import { AppRegistry } from 'react-native';
 
-// Register the app
-AppRegistry.registerComponent('main', () => App);
-
-// Initialize the app for web
-const rootTag = document.getElementById('root');
-const root = createRoot(rootTag);
-const RootComponent = AppRegistry.getApplication('main').element;
-root.render(RootComponent);
+registerRootComponent(App);
